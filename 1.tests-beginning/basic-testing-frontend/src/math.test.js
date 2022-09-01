bb4859b81472bde11d9290df8b25924ae2eccc04
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-09-01 13:55:27 +0200

    Define behavior & fixing errors in my code

--- 1.tests-beginning/basic-testing-frontend/src/math.test.js
+++ 1.tests-beginning/basic-testing-frontend/src/math.test.js
@@ -3,7 +3,7 @@ import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
   // Arrange
-  const numbers = [1, 2, 3];
+  const numbers = [1, 2];
 
   // Act
   const result = add(numbers);
@@ -14,5 +14,25 @@ it("should summarize all number values in an array", () => {
     0
   );
 
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield NaN if a least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + +curValue,
+    0
+  );
   expect(result).toBe(expectedResult);
 });
