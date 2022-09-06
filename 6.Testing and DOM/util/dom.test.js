67601e7e41079704eee6ffba795ce7b85c42031a
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-09-06 21:37:17 +0200

    Test for virtual dom

--- 6.Testing and DOM/util/dom.test.js
+++ 6.Testing and DOM/util/dom.test.js
@@ -1,7 +1,35 @@
-import { it } from "vitest";
+import fs from "fs";
+import path from "path";
+
+import { beforeEach, expect, it, vi } from "vitest";
+import { Window } from "happy-dom";
 
 import { showError } from "./dom";
 
+const htmlDocPath = path.join(process.cwd(), "index.html");
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+vi.stubGlobal("document", document);
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+  document.write(htmlDocumentContent);
+});
+
 it("first test", () => {
   showError("test");
+
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull();
+});
+
+it("should not contain an error paragraph initially", () => {
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).toBeNull();
 });
