110965d864c5acf48d718cefdf7b530cc4302f21
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-09-06 18:35:03 +0200

    Add local mock values

---
+++ 5.more-on-mocking/posts/post.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, it } from "vitest";
+import { extractPostData } from "./posts";
+
+const testTitle = "Test title";
+const testContent = "Test content";
+let testFormData;
+
+describe("extractPostData()", () => {
+  beforeEacth(() => {
+    testFormData = {
+      title: testTitle,
+      content: testContent,
+      get(identifier) {
+        return this[identifier];
+      },
+    };
+  });
+  it("should extract title and content from the provided form data", () => {
+    const data = extractPostData(testFormData);
+
+    expect(data.title).toBe(testTitle);
+    expect(data.content).toBe(testContent);
+  });
+});
